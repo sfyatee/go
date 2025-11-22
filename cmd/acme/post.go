b177e300658541f48017147a60b4d338fb076135
demian garcia <dag@demians.org>
2025-11-22 11:25:24 -0800

    cmd/acme: use mux9p for post9pservice

--- cmd/acme/post.go
+++ cmd/acme/post.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"9fans.net/go/plan9/client"
+	"9fans.net/go/plan9/mux9p"
 )
 
 var chattyfuse int
+
+type stdio struct{ r, w *os.File }
+
+func (rw *stdio) Read(p []byte) (int, error)  { return rw.r.Read(p) }
+func (rw *stdio) Write(p []byte) (int, error) { return rw.w.Write(p) }
 
 func post9pservice(rfd, wfd *os.File, name, mtpt string) error {
 	if name == "" && mtpt == "" {
@@ -20,23 +26,23 @@ func post9pservice(rfd, wfd *os.File, name, mtpt string) error {
 	}
 
 	if name != "" {
-		var addr string
+		var network, addr string
 		if strings.Contains(addr, "!") { // assume is already network address
-			addr = name
+			part := strings.SplitN(name, "!", 2)
+			network, addr = part[0], part[1]
 		} else {
-			addr = "unix!" + client.Namespace() + "/" + name
+			network = "unix"
+			addr = filepath.Join(client.Namespace(), name)
 		}
-		cmd := exec.Command("9pserve", "-u", addr)
-		cmd.Stdin = rfd
-		cmd.Stdout = wfd
-		cmd.Stderr = os.Stderr
-		err := cmd.Run()
-		if err != nil {
-			return err
-		}
+		go func() {
+			err := mux9p.Listen(network, addr, &stdio{r: rfd, w: wfd}, &mux9p.Config{})
+			if err != nil {
+				log.Printf("9p multiplexer failed: %v", err)
+			}
+		}()
 		if mtpt != "" {
-			// reopen
-			log.Fatalf("post9pservice mount not implemented")
+ 			// reopen
+ 			log.Fatalf("post9pservice mount not implemented")
 		}
 	}
 	if mtpt != "" {
