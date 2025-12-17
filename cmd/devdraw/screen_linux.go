b353db18b78ea533b44a013e3be1141054a8aeed
demian garcia <dag@demians.org>
2025-12-17 12:26:23 -0800

    .

--- cmd/devdraw/screen_linux.go
+++ cmd/devdraw/screen_linux.go
@@ -402,7 +402,34 @@ func (impl *theImpl) Key(win *window.Window, in *window.Input, time uint32, key 
 	gfx_keystroke(impl.client, ch)
 }
 
+const (
+	// From linux/input-event-codes.h
+	BtnLeft   = 0x110
+	BtnRight  = 0x111
+	BtnMiddle = 0x112
+
+	P9Mouse1 = 1
+	P9Mouse2 = 2
+	P9Mouse3 = 4
+)
+
 func (impl *theImpl) Button(w *window.Widget, in *window.Input, time uint32, button uint32, state wl.PointerButtonState, data window.WidgetHandler) {
+	var m int
+	switch button {
+	case BtnLeft:
+		m = P9Mouse1
+	case BtnMiddle:
+		m = P9Mouse2
+	case BtnRight:
+		m = P9Mouse3
+	default:
+		m = 0
+	}
+	if state == wl.PointerButtonStatePressed {
+		impl.mouse.Buttons |= m
+	} else {
+		impl.mouse.Buttons &^= m
+	}
 	impl.mouse.Msec = time
 	gfx_mousetrack(impl.client, impl.mouse.X, impl.mouse.Y, impl.mouse.Buttons, impl.mouse.Msec)
 }
